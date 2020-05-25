6fe6c49c85263f64f242688a7643cce6bd402416
chisoftltd <chisoftltd@gmail.com>
2020-05-25 16:03:46 +0200

    Add project files.

---
+++ PythonOperatorConditional/PythonOperatorConditional.py
@@ -0,0 +1,88 @@
+# Program by ChisoftMedia
+
+# Python Arithmetic Operator
+
+x = 10
+y = 2 * x
+z = (x == y)
+
+print(z)
+
+del z
+
+
+z = (x**y)
+
+print(z)
+
+
+# Python Comparison/Relational Operators
+
+z = (y//x)
+print(z)
+
+z = (x != y)
+
+print(z)
+
+
+z = (x != y)
+
+print(z)
+
+
+z = (x > y)
+
+print(z)
+
+
+z = (x < y)
+
+print(z)
+
+
+z = (x >= y)
+
+print(z)
+
+
+z = (x <= y)
+
+print(z)
+
+
+# Python Assignment Operators
+
+x = 10
+y = 2 * x
+x += y
+
+print(x)
+
+x -= y
+
+print(x)
+
+x *= y
+
+print(x)
+
+x /= y
+
+print(x)
+
+x %= y
+
+print(x)
+
+x **= y
+
+print(x)
+
+x //= y
+
+print(x)
+
+# Python Bitwise Operators
+
+
